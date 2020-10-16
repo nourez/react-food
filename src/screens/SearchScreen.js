4ee24c5e4e80f070a74472aaae1bfd967cb15029
Nourez Rawji <nourez@nourez.dev>
2020-10-16 18:34:53 -0400

    Created styling for SearchBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar/>
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
