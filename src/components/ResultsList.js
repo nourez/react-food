8d2c1eb4a2758238c60d260622c339ed3dd2c00c
Nourez Rawji <nourez@nourez.dev>
2020-10-20 17:17:47 -0400

    Display restaurant details

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import ResultsDetail from "./ResultsDetail";
+import { withNavigation } from 'react-navigation'
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
@@ -11,8 +12,14 @@ const ResultsList = ({ title, results }) => {
         showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={result => result.id}
-        renderItem={({item})  => {
-          return <ResultsDetail result={item}/>
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ResultsShow", {id: item.id})}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
@@ -31,4 +38,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
